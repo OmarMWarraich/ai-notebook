4c05f2f54b98717522c85d2851e6c3aa4a0d2467
OmarMWarraich <earthling986@gmail.com>
2023-10-15 17:59:03 +0500

    feat: Create Notebook API using React Query

--- src/components/CreateNoteDialogue.tsx
+++ src/components/CreateNoteDialogue.tsx
@@ -1,15 +1,51 @@
 'use client';
 
-import React from 'react'
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
+import React from 'react';
+import axios from 'axios';
 import { Plus } from 'lucide-react';
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
+import { create } from 'domain';
 
 type Props = {}
 
 const CreateNoteDialogue = (props: Props) => {
     const [input, setInput] = React.useState('')
+    const router = useRouter()
+    const createNoteBook = useMutation({
+        mutationFn: async () => {
+            const response = await axios.post('/api/createNotebook', {
+                name: input
+            })
+            return response.data
+        }
+    })
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        
+        if (input === '') {
+            window.alert('Please enter a name for the notebook')
+            return
+        }
+
+        createNoteBook.mutate(undefined, {
+            onSuccess: ({note_id}) => {
+                console.log("created new note:",{note_id})
+                router.push(`/notebook/${note_id}`)
+            },
+            onError: (error) => {
+                console.error(error)
+            }
+        })
+    }
+
+
+
   return (
     <Dialog>
         <DialogTrigger>
@@ -29,7 +65,7 @@ const CreateNoteDialogue = (props: Props) => {
                     You can create a new dialog by clicking the button below.
                 </DialogDescription>
             </DialogHeader>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Name..." />
                 <div className="h-4"></div>
                 <div className="flex items-center gap-2">
